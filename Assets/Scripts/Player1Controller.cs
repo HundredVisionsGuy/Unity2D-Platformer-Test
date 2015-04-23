a70abbcaa4d11a2844241b92db5be994a92ed58c
hundredvisionsguy <winikkc@hsd.k12.or.us>
2015-04-23 16:07:32 -0700

    Added controller script, physics, & adjustments

---
+++ Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Controller : MonoBehaviour {
+
+	// Movement
+	public float speed;
+	public float jump;
+	float moveVelocity;
+	bool grounded = false;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		// Jump
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.UpArrow) ||
+			Input.GetKeyDown (KeyCode.Z) || Input.GetKeyDown (KeyCode.W)) 
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+		}
+
+		moveVelocity = 0;
+
+		// Horizontal Movement
+		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A) )
+	    {
+				moveVelocity = -speed;
+		}
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) 
+		{
+				moveVelocity = speed;
+		}
+
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+	// Check if grounded
+	void OnTriggerEnter2D()
+	{
+		grounded = true;
+	}
+	void OnTriggerExit2D()
+	{
+		grounded = false;
+	}
+
+}

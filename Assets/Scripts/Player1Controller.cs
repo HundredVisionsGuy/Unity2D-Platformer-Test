50f91fc3d78c58e3c588ca06a90154a88b919712
hundredvisionsguy <winikkc@hsd.k12.or.us>
2015-04-24 10:09:32 -0700

    Added jump feature (W)

---
+++ Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Controller : MonoBehaviour {
+
+	// Movement Variables
+	public float speed;
+	public float jump;
+	float moveVelocity;
+	bool grounded = false; 
+	
+	void Update () {
+		// Jump
+		if (Input.GetKeyDown (KeyCode.W)) 
+		{
+			// change the velocity of the rigidbody2D
+			GetComponent<Rigidbody2D> ().velocity = 
+				new Vector2(GetComponent<Rigidbody2D>().velocity.x,
+				            jump);
+		}
+		moveVelocity = 0;
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (
+			moveVelocity, 
+			GetComponent<Rigidbody2D> ().velocity.y);
+
+	}
+}

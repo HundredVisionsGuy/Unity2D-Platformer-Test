a47f2e9d6bdadb017daf58bccfc100b18a199a9a
hundredvisionsguy <winikkc@hsd.k12.or.us>
2015-05-04 10:03:08 -0700

    added horizontal movement & can't jump when not grounded

--- Assets/Scripts/Player1Controller.cs
+++ Assets/Scripts/Player1Controller.cs
@@ -11,17 +11,37 @@ public class Player1Controller : MonoBehaviour {
 	
 	void Update () {
 		// Jump
-		if (Input.GetKeyDown (KeyCode.W)) 
+		if (Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.Space)) 
 		{
-			// change the velocity of the rigidbody2D
-			GetComponent<Rigidbody2D> ().velocity = 
+			// change the velocity of the rigidbody2D (ONLY if Grounded)
+			if (grounded) {
+				GetComponent<Rigidbody2D> ().velocity = 
 				new Vector2(GetComponent<Rigidbody2D>().velocity.x,
 				            jump);
+			}
 		}
 		moveVelocity = 0;
+
+		// Horizontal Movement
+		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) {
+			// move left
+			moveVelocity = -speed;
+		}
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) {
+			// move right
+			moveVelocity = speed;
+		}
+		// prevent character from sliding
 		GetComponent<Rigidbody2D> ().velocity = new Vector2 (
 			moveVelocity, 
 			GetComponent<Rigidbody2D> ().velocity.y);
-
 	}
+	// Check if grounded
+	void OnTriggerEnter2D() {
+		grounded = true;
+	}
+	void OnTriggerExit2D() {
+		grounded = false;
+	}
+
 }

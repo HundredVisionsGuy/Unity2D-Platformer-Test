f9d90f916d08318c1bc6c1da40a0f57987905114
hundredvisionsguy <winikkc@hsd.k12.or.us>
2015-05-06 15:34:58 -0700

    adjusted script

--- Assets/Scripts/Player1Controller.cs
+++ Assets/Scripts/Player1Controller.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class Player1Controller : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+	// Movement Variables
+	public float speed;
+	public float jump;
+	float moveVelocity;
+	bool grounded = false;
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// Jump
+		if (Input.GetKeyDown (KeyCode.Space) ||
+			Input.GetKeyDown (KeyCode.W)) {
+			if (grounded) {
+				GetComponent<Rigidbody2D>().velocity =
+					new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+			}
+		}
 	}
+
+	// Check if grounded
+	void OnTriggerEnter2D() {
+		grounded = true;
+	}
+	void onTriggerExit2D() {
+		grounded = false;
+	}
+
 }

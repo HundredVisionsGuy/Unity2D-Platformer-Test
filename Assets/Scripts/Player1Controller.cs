05fdc95437a3ef6d2e1a80492c3663e2788ded37
hundredvisionsguy <winikkc@hsd.k12.or.us>
2015-05-08 13:00:35 -0700

    Added physics jumping and left and right movement

--- Assets/Scripts/Player1Controller.cs
+++ Assets/Scripts/Player1Controller.cs
@@ -3,45 +3,39 @@ using System.Collections;
 
 public class Player1Controller : MonoBehaviour {
 
-	// Movement Variables
+	// Movement variables
 	public float speed;
 	public float jump;
 	float moveVelocity;
-	bool grounded = false; 
-	
+	bool grounded = false;
+
+	// Update is called once per frame
 	void Update () {
 		// Jump
-		if (Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.Space)) 
-		{
-			// change the velocity of the rigidbody2D (ONLY if Grounded)
+		if ( Input.GetKeyDown (KeyCode.Space) || 
+		    Input.GetKeyDown (KeyCode.W)) {
 			if (grounded) {
-				GetComponent<Rigidbody2D> ().velocity = 
-				new Vector2(GetComponent<Rigidbody2D>().velocity.x,
-				            jump);
+				GetComponent<Rigidbody2D>().velocity = new Vector2(
+				GetComponent<Rigidbody2D>().velocity.x, jump);
 			}
 		}
 		moveVelocity = 0;
 
 		// Horizontal Movement
-		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) {
-			// move left
-			moveVelocity = -speed;
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+			moveVelocity = -speed; // move left
 		}
-		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) {
-			// move right
-			moveVelocity = speed;
+		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
+			moveVelocity = speed; // move left
 		}
-		// prevent character from sliding
-		GetComponent<Rigidbody2D> ().velocity = new Vector2 (
-			moveVelocity, 
-			GetComponent<Rigidbody2D> ().velocity.y);
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveVelocity,
+		                                    GetComponent<Rigidbody2D> ().velocity.y);
 	}
-	// Check if grounded
+	// Check to see if grounded or not
 	void OnTriggerEnter2D() {
 		grounded = true;
 	}
 	void OnTriggerExit2D() {
 		grounded = false;
 	}
-
 }
